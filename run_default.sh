7cb81ecb3aebcb527b6ed79d029e8ef198dc71da
k0tyak1t <konstantin.shiyanenko@gmail.com>
2025-02-15 21:26:27 +0300

    extra files untracked

--- run_default.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-mkdir -p build geometry basis
-rm -f build/scf.out
-g++ src/*.cpp -Iinclude -llapack -lboost_math_c99 -L/usr/lib/x86_64-linux-gnu -o build/scf.out -O3
-time ./build/scf.out geometry/h2o.xyz basis/6311.basis
-exit 0
